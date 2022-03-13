50f1db84268d5f42424802aad46ed6c3cae5f17e
Ricardo Lence <rlence@MacBook-Air-de-Ricardo.local>
2022-03-13 16:17:20 +0100

    refactor: jwt config, env example

--- back/src/config/config.js
+++ back/src/config/config.js
@@ -1,6 +1,7 @@
 const Enviroment = {
     PORT: process.env.PORT,
-    ENVIROMENT: process.env.NODE_ENV
+    ENVIROMENT: process.env.NODE_ENV,
+    KEY_TOKEN: process.env.KEY_TOKEN,
 }
 
 export default Enviroment;
--- back/src/middelware/jwt.js
+++ back/src/middelware/jwt.js
@@ -1,10 +1,13 @@
 import jwt from "jsonwebtoken";
+import Enviroment from "../config/config";
+
+const { KEY_TOKEN } = Enviroment;
 
 export function generateToken(user){
-    const token = jwt.sign({user}, process.env.KEY_TOKEN);
+    const token = jwt.sign({user}, KEY_TOKEN);
     return token;
 }
  
 export function decodeToken(token){
-    return jwt.verify(token, process.env.KEY_TOKEN); 
+    return jwt.verify(token, KEY_TOKEN); 
 }
