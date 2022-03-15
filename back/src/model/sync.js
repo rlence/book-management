3899f0a341b74b258624ec94bbeae4509121d549
Ricardo Lence <rlence@MacBook-Air-de-Ricardo.local>
2022-03-15 18:25:23 +0100

    refactor: relationship between the models

--- back/src/model/sync.js
+++ back/src/model/sync.js
@@ -2,22 +2,24 @@ import {Author, Editorial, User, Book, Bookings} from "./index";
 
 export const asyncModel =  async () => {
     try{
+        Bookings.belongsTo(User, { foreingKey: 'user_id' , sourceKey: 'id'});
+        Bookings.belongsTo(Book, { foreingKey: 'book_id' , sourceKey: 'id'});
+        
+        Author.hasMany(Book, { foreingKey: 'author_id' , sourceKey: 'id'});
+        Editorial.hasMany(Book, { foreingKey: 'editorial_id' , sourceKey: 'id'});
+
+        User.hasMany(Bookings, { foreingKey: 'user_id' , sourceKey: 'id'});
+        Book.hasMany(Bookings, { foreingKey: 'book_id' , sourceKey: 'id'});
+        
+        Book.belongsTo(Author, { foreingKey: 'author_id' , sourceKey: 'id'});
+        Book.belongsTo(Editorial, { foreingKey: 'editorial_id' , sourceKey: 'id'});
+
         await User.sync({ force: true });
         await Author.sync({ force: true });
         await Editorial.sync({ force: true });
         await Book.sync({ force: true });
         await Bookings.sync({ force: true });
 
-        Bookings.hasMany(User, { foreingKey: 'user_id' , sourceKey: 'id'});
-        Bookings.hasMany(Book, { foreingKey: 'book_id' , sourceKey: 'id'});
-        Author.hasMany(Book, { foreingKey: 'author_id' , sourceKey: 'id'});
-        Editorial.hasMany(Book, { foreingKey: 'editorial_id' , sourceKey: 'id'});
-
-        User.belongsTo(Bookings, { foreingKey: 'user_id' , sourceKey: 'id'});
-        Book.belongsTo(Bookings, { foreingKey: 'book_id' , sourceKey: 'id'});
-        Book.hasMany(Author, { foreingKey: 'author_id' , sourceKey: 'id'});
-        Book.hasMany(Editorial, { foreingKey: 'editorial_id' , sourceKey: 'id'});
-        
         console.info("[INFO SYNC TABLES]")
     }catch(err){
         console.log("[ERROR ASYNC MODEL]:", err)
