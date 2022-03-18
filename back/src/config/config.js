f73e412e5bf5b72e5740bd7349993cc6c7404cca
Ricardo Lence <rlence@MacBook-Air-de-Ricardo.local>
2022-03-18 21:30:13 +0100

    refactor: change config and .env

--- back/src/config/config.js
+++ back/src/config/config.js
@@ -1,7 +1,5 @@
 const Enviroment = {
     PORT: process.env.PORT,
-    ENVIROMENT: process.env.NODE_ENV,
-    KEY_TOKEN: process.env.KEY_TOKEN,
     POSTGRES_USER: process.env.POSTGRES_USER,
     POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
     POSTGRES_PORT: process.env.POSTGRES_PORT,
